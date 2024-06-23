2f2308fa67ad22b571b4f744d80ec1ec868f0d54
Johana Carrizo <johanacarrizo20@gmail.com>
2024-06-23 19:50:00 -0300

    Build:created structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Categories from "./components/Category/Categories"
+import Hero from "./components/Hero/Hero"
+import Footer from "./components/Footer/Footer"
+import CardsProducts from "./components/Product/CardsProducts"
+import Layout from "./components/Layout/Layout"
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Layout>
+        <Hero/>
+        <Categories/>
+        <CardsProducts/>
+      </Layout>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
